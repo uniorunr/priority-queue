2b3dd9f12538fbe73a42af261de9dc0c14b6758b
uniorunr <gagael23@live.com>
2019-02-27 02:36:39 +0300

    feat: pass all tests for Node class

--- src/max-heap.js
+++ src/max-heap.js
@@ -2,15 +2,23 @@ const Node = require('./node');
 
 class MaxHeap {
 	constructor() {
-		
+		this.root = null;
+		this.parentNodes = [];
 	}
 
 	push(data, priority) {
-		
+		var node = {
+			priority,
+			data,
+		}
+		this.insertNode(node);
+		this.shiftNodeUp(priority);
 	}
 
 	pop() {
-		
+		if (this.root) {
+			this.detachRoot();
+		}
 	}
 
 	detachRoot() {
@@ -30,11 +38,16 @@ class MaxHeap {
 	}
 
 	clear() {
-		
+		this.root = null;
+		this.parentNodes = [];
 	}
 
 	insertNode(node) {
-		
+		if (!this.root) {
+			this.root = node;
+		} else {
+			this.parentNodes.push(node);
+		}
 	}
 
 	shiftNodeUp(node) {
--- src/node.js
+++ src/node.js
@@ -1,22 +1,77 @@
 class Node {
 	constructor(data, priority) {
-
+		this.data = data;
+		this.priority = priority;
+		this.parent = null;
+		this.left = null;
+		this.right = null;
 	}
 
 	appendChild(node) {
-
+		if (this.left && this.right) return;
+		if (this.left) {
+			this.right = node;
+			node.parent = this;
+		} else {
+			this.left = node;
+			node.parent = this;
+		}
 	}
 
 	removeChild(node) {
-
+		if (node === this.left) {
+			this.left = null;
+			node.parent = null;
+		} else if (node === this.right) {
+			this.right = null;
+			node.parent = null;
+		} else throw new Error('error');
 	}
 
 	remove() {
-
+		if (!this.parent) return;
+		this.parent.removeChild(this);
 	}
 
 	swapWithParent() {
-		
+		if (!this.parent) return;
+		const grandFather = this.parent.parent;
+		const parent = this.parent;
+		const parentLeft = this.parent.left;
+		const parentRight = this.parent.right;
+		const left = this.left;
+		const right = this.right;
+		this.parent = grandFather;
+		parent.parent = this;
+		if (parent.right === this) {
+			this.right = parent;
+			this.left = parent.left;
+			parent.parent = this;
+			parent.left = left;
+			parent.right = right;
+			if (parentLeft) {
+				parentLeft.parent = this;
+			}
+			if (grandFather && grandFather.right === parent) {
+				grandFather.right = this;
+			} else if (grandFather && grandFather.left === parent) {
+				grandFather.left = this;
+			}
+		} else if (parent.left === this) {
+			this.left = parent;
+			this.right = parent.right;
+			parent.parent = this;
+			parent.left = left;
+			parent.right = right;
+			if (parentRight) {
+				parentRight.parent = this;
+			}
+			if (grandFather && grandFather.right === parent) {
+				grandFather.right = this;
+			} else if (grandFather && grandFather.left === parent) {
+				grandFather.left = this;
+			}
+		} 
 	}
 }
 
