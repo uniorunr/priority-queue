f386173f2b850814c272282ccd72ace0c2f3a858
uniorunr <gagael23@live.com>
2019-03-24 16:02:08 +0300

    feature: implement conastructor and part of the push method in queue

--- src/queue.js
+++ src/queue.js
@@ -2,11 +2,12 @@ const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
 	constructor(maxSize) {
-
+		this.maxSize = maxSize || 30;
+		this.heap = new MaxHeap();
 	}
 
 	push(data, priority) {
-
+		this.heap.push(data, priority);
 	}
 
 	shift() {
